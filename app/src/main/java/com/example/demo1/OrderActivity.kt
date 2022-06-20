27e68e360d78296cee8b3e6e5579f7ae953f521b
ryukakusan99 <al20099@shibaura-it.ac.jp>
2022-06-20 22:30:18 +0900

    #fuji-6

--- app/src/main/java/com/example/demo1/OrderActivity.kt
+++ app/src/main/java/com/example/demo1/OrderActivity.kt
@@ -15,9 +15,29 @@ class OrderActivity : AppCompatActivity() , View.OnClickListener{
 
         val order : Button = findViewById(R.id.order)
         val billstart : Button = findViewById(R.id.billstart)
+        val menu1 : Button = findViewById(R.id.menu1)
+        val menu2 : Button = findViewById(R.id.menu2)
+        val menu3 : Button = findViewById(R.id.menu3)
+        val menu4 : Button = findViewById(R.id.menu4)
+        val menu5 : Button = findViewById(R.id.menu5)
+        val menu6 : Button = findViewById(R.id.menu6)
+        val menu7 : Button = findViewById(R.id.menu7)
+        val menu8 : Button = findViewById(R.id.menu8)
+        val menu9 : Button = findViewById(R.id.menu9)
+        val menu10 : Button = findViewById(R.id.menu10)
 
         order.setOnClickListener(this)
         billstart.setOnClickListener(this)
+        menu1.setOnClickListener(this)
+        menu2.setOnClickListener(this)
+        menu3.setOnClickListener(this)
+        menu4.setOnClickListener(this)
+        menu5.setOnClickListener(this)
+        menu6.setOnClickListener(this)
+        menu7.setOnClickListener(this)
+        menu8.setOnClickListener(this)
+        menu9.setOnClickListener(this)
+        menu10.setOnClickListener(this)
 
     }
 
