d34d95242c606c9a3c826add3d00f2d52a14afa9
yasu <al20016@shibaura-it.ac.jp>
2022-06-25 00:34:44 +0900

    QRできた

--- app/src/main/java/com/example/demo1/QrActivity.kt
+++ app/src/main/java/com/example/demo1/QrActivity.kt
@@ -1,40 +1,44 @@
 package com.example.demo1
 
-import android.R
+
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 
-class QrActivity : Activity() {
+class QrActivity : AppCompatActivity() ,View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.example.demo1.R.layout.activity_calorie)
-        imageDisplayInitialize()
+        setContentView(R.layout.activity_calorie)
+
+        val displayButton : Button = findViewById(R.id.button1)//ボタン宣言
+        val hideButton : Button = findViewById(R.id.button2)//ボタン宣言
+        displayButton.setOnClickListener(this)
+        hideButton.setOnClickListener(this)
 
     }
 
+    //ボタンにOnClickListenerインターフェースを実装
+    //表示用ボタンの機能実装
 
-    // 表示非表示ボタンの実装
-    private fun imageDisplayInitialize() {
-        //クリックイベントを取得するボタン
-        val displayButton = findViewById<View>(R.id.button1) as Button
-        val hideButton = findViewById<View>(R.id.button2) as Button
-
-        //画像表示
-        val imageView = findViewById<ImageView>(com.example.demo1.R.id.Image)
-        imageView.setImageResource(R.drawable.stat_sys_data_bluetooth)
-
-        //ボタンにOnClickListenerインターフェースを実装
-        //表示用ボタンの機能実装
-        displayButton.setOnClickListener {
-            imageView.visibility = View.VISIBLE
-        }
-        //非表示用ボタンの機能実装
-        hideButton.setOnClickListener {
-            imageView.visibility = View.INVISIBLE
+    @SuppressLint("SetTextI18n")
+    override fun onClick(view: View) {
+        val imageView = findViewById<ImageView>(R.id.Image)//写真宣言
+        imageView.setImageResource(R.drawable.test)
+        when (view.id) {
+            R.id. button1-> {
+                imageView.visibility = View.VISIBLE
+            }
+            //非表示用ボタンの機能実装
+            R.id.button2 -> {
+                val intent = Intent(this@QrActivity, OrderActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
