55b9915cc0f885e567d8c2549833757652c87f5f
ymd1138 <74826010+ymd1138@users.noreply.github.com>
2022-07-05 03:42:44 +0900

    データベース接続直前

--- app/src/main/java/com/example/demo1/LoginActivity.kt
+++ app/src/main/java/com/example/demo1/LoginActivity.kt
@@ -17,22 +17,28 @@ package com.example.demo1
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 
 //ログイン画面
-class MainActivity : AppCompatActivity()  ,View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var i: Int? = 0
+
     //宣言
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rogin)
         val button: Button = findViewById(R.id.login)//ログインボタン宣言
         button.setOnClickListener(this)
+    }
+
+    public fun set(i: Int) {
+        this.i = i;
     }
 
     //入力確認
@@ -52,18 +58,23 @@ class MainActivity : AppCompatActivity()  ,View.OnClickListener {
                     Toast.makeText(this, "パスワードを入力してください。", Toast.LENGTH_SHORT).show()
                 } else {
 
-                    // if(username==id && password ==pas) {
+                    // val a = arrayOf(username, password)
+                    val b: String = username.text.toString()
+                    val task = DBLogin(this@MainActivity)
+                    task.execute(b)
+                    val str : String = this.i.toString();
+                    Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+                }
+                if (this.i == 1) {
                     Toast.makeText(this, "ログイン", Toast.LENGTH_SHORT).show()
                     val intent = Intent(this@MainActivity, OrderActivity::class.java)
                     startActivity(intent)
-                    //}else{
-                    //  Toast.makeText(this, "IDかパスワードが違います", Toast.LENGTH_SHORT).show()
-                    //}
+                } else {
+                    Toast.makeText(this, "IDかパスワードが違います", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
 }
-    // }
 //}
 
